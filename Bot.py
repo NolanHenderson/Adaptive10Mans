2b600a43100c3c697ed611a87087aa58af2cc7b8
NolanHenderson <nth22a@acu.edu>
2024-08-15 15:46:10 -0500

    better sanitization of incoming data, and region based queueing

--- Bot.py
+++ Bot.py
@@ -8,23 +8,32 @@ from typing import Optional
 
 import discord
 from discord.ext import commands
+from discord.utils import get
 from replit.object_storage import Client
 
-client = Client() # Create a client instance
-queue_pc = {}
-queue_console = {}
+client = Client()  # Create a client instance
+#queue_pc = {}
+#queue_console = {}
 #queue_dict = {}
+
 role_dict = {}
 full_queue = None
 match_size = 10
 members = []
-list_of_regions = ["Asia East", "Asia South East", "Japan East", "South Africa North",
-                   "UAE North", "EU West", "EU North", "US East", "US Central",
-                   "US West","US South Central", "Brazil South", "Australia East"]
-list_of_ranks = ["C1","C2","C3","C4","C5","B1","B2","B3","B4","B5","S1","S2","S3","S4",
-                 "S5","G1","G2","G3","G4","G5","P1","P2","P3","P4","P5","D1","D2","D3",
-                 "D4","D5", "F"]
+list_of_regions = [
+    "Asia East", "Asia South East", "Japan East", "South Africa North",
+    "UAE North", "EU West", "EU North", "US East", "US Central", "US West",
+    "US South Central", "Brazil South", "Australia East"
+]
+list_of_ranks = [
+    "C1", "C2", "C3", "C4", "C5", "B1", "B2", "B3", "B4", "B5", "S1", "S2",
+    "S3", "S4", "S5", "G1", "G2", "G3", "G4", "G5", "P1", "P2", "P3", "P4",
+    "P5", "E1", "E2", "E3", "E4", "E5", "D1", "D2", "D3", "D4", "D5", "F"
+]
 list_of_systems = ["PC", "PS", "Xbox"]
+
+list_of_major_regions = ["ja", "so", "ua", "eu", "us", "br", "au", "af"]
+queue_regions = {region: {'pc': {}, 'console': {}} for region in list_of_major_regions}
 
 intents = discord.Intents.default()
 intents.members = True  # Enable member intents (to see members in guilds)
@@ -70,6 +79,7 @@ class Player:
                    rank=data['rank'],
                    region=data['region'],
                    system=data['system'])
+
     @classmethod
     def sanitize_filename(cls, filename):
         # Remove leading/trailing whitespace and replace spaces with underscores
@@ -193,71 +203,84 @@ async def setup_profiles(ctx):
     guild = ctx.guild
     new_user = ctx.author
 
-
     async def ask_user_for_input(member, question, input_type=str, timeout=60):
         await member.send(question)
         print(f"Sent question to {member.name}: {question}")
 
         def check(m):
-            return m.author == member and isinstance(m.channel, discord.DMChannel)
+            return m.author == member and isinstance(m.channel,
+                                                     discord.DMChannel)
 
         try:
             msg = await bot.wait_for('message', check=check, timeout=timeout)
             print(f"Received response from {member.name}: {msg.content}")
             return input_type(msg.content)
         except asyncio.TimeoutError:
-            await member.send("You took too long to respond! Please try again.")
+            await member.send("You took too long to respond! Please try again."
+                              )
             print(f"Timeout waiting for response from {member.name}")
             return None
 
     try:
-        await new_user.send(f"Hello {new_user}, I am a bot from {guild}. Let's set up your player profile!")
+        await new_user.send(
+            f"Hello {new_user}, I am a bot from {guild}. Let's set up your player profile!"
+        )
         print(f"Greeting message sent to {new_user.name}")
 
-        ubi_name = await ask_user_for_input(new_user, "Please enter your Ubisoft account name:")
+        ubi_name = await ask_user_for_input(
+            new_user, "Please enter your Ubisoft account name:")
         if ubi_name is None: return
         dis_name = new_user.name
         print(f"Collected Ubisoft name: {ubi_name}")
 
-        region = await ask_user_for_input(new_user,
-                                          "Please enter your region from this list:\n"
-                                          "(Asia East, Asia South East, Japan East, South Africa North, "
-                                          "UAE North, EU West, EU North, US East, US Central, US West, "
-                                          "US South Central, Brazil South, Australia East)")
-        if region in list_of_regions:
+        region = await ask_user_for_input(
+            new_user, "Please enter your region from this list:\n"
+            "(Asia East, Asia South East, Japan East, South Africa North, "
+            "UAE North, EU West, EU North, US East, US Central, US West, "
+            "US South Central, Brazil South, Australia East)")
+        if region.lower() in [r.lower() for r in list_of_regions]:
             print(f"Collected region: {region}")
         else:
-            await new_user.send("Invalid region. Please use !setup profile again.")
+            await new_user.send(
+                "Invalid region. Please use !setup profile again.")
             return
 
-        system = await ask_user_for_input(new_user, "Please enter your system:\n"
-                                          '(PC, Xbox, PS. if you play on multiple, say: "PC PS")')
-        if system in list_of_systems:
+        system = await ask_user_for_input(
+            new_user, "Please enter your system:\n"
+            '(PC, Xbox, PS. if you play on multiple, say: "PC PS")')
+        if system.lower() in [stm.lower() for stm in list_of_systems]:
             pass
         else:
-            await new_user.send("Invalid system. Please use !setup profile again.")
+            await new_user.send(
+                "Invalid system. Please use !setup profile again.")
             return
 
-        rank = await ask_user_for_input(new_user, "Please enter your rank:\n"
-                                        "(Use the form G1 for Gold 1, P3 for Plat 3, etc... Use D1 for champ.)")
-        if rank in list_of_ranks:
+        rank = await ask_user_for_input(
+            new_user, "Please enter your rank:\n"
+            "(Use the form G1 for Gold 1, P3 for Plat 3, etc... Use D1 for champ.)"
+        )
+        if rank.lower() in [rnk.lower() for rnk in list_of_ranks]:
             print(f"Collected rank: {rank}")
         else:
-            await new_user.send("Invalid rank. Please use !setup profile again.")
+            await new_user.send(
+                "Invalid rank. Please use !setup profile again.")
             return
 
         elo = 1500
         wins = 0
         losses = 0
 
-        player = Player(ubi_name, dis_name, elo, wins, losses, rank, region, system)
+        player = Player(ubi_name, dis_name, elo, wins, losses, rank, region,
+                        system)
         player.save_to_json(ctx.guild.id)
         print(f"Saved player profile for {new_user.name}")
 
-        await new_user.send("Your profile has been successfully created and saved!")
+        await new_user.send(
+            "Your profile has been successfully created and saved!")
     except Exception as e:
         print(f"An error occurred: {e}")
-        await new_user.send(f"An error occurred: {e}\n\nPlease reach out to a moderator")
+        await new_user.send(
+            f"An error occurred: {e}\n\nPlease reach out to a moderator")
 
 
 @bot.event
@@ -280,16 +303,21 @@ async def Join(ctx, role: Optional[str] = "Flex"):
     role_dict[user] = role
     print(role)
 
-    # Fetch player's system
+    # Fetch player's system and region
     player = check_profile(server_id, user)
     if not player:
         await ctx.send("No user profile, make one with !setup_profile")
         return
 
-    system = player.system
+    system = player.system.lower()
+    major_region = player.region[:2].lower()
+    if major_region not in list_of_major_regions:
+        await ctx.send("Invalid region in profile. Please update your profile with a valid region.")
+        return
 
     # Determine the correct queue
-    queue = queue_pc if "PC" in system else queue_console
+    queues = queue_regions[major_region]
+    queue = queues['pc'] if 'pc' in system else queues['console']
 
     # Create the queue if it doesn't exist
     if server_id not in queue:
@@ -298,9 +326,9 @@ async def Join(ctx, role: Optional[str] = "Flex"):
     # Add user to the queue if not already in it
     if user not in queue[server_id]:
         queue[server_id].append(user)
-        await ctx.send(f"{user.mention} has joined the queue for {system} players!")
+        await ctx.send(f"{user.mention} has joined the queue for {system} players in {major_region.upper()} region!")
     else:
-        await ctx.send(f"{user.mention}, you're already in the queue.")
+        await ctx.send(f"{user.mention}, you're already in the queue for {player.region} region.")
 
     # Try to make a match
     if len(queue[server_id]) >= match_size:
@@ -310,16 +338,8 @@ async def Join(ctx, role: Optional[str] = "Flex"):
             bot.loop.create_task(
                 create_lobby(ctx, "Blue Team DEF", "Orange Team ATK", members))
         queue[server_id] = [
-            user for user in queue.get(server_id, [])
-            if user not in members
+            user for user in queue.get(server_id, []) if user not in members
         ]
-
-
-#
-#
-#
-#
-
 
 # Subcommand for leaving the queue
 @Queue.command(name="Leave", aliases=['leave', 'l', 'L', 'LEAVE'])
@@ -327,36 +347,46 @@ async def Leave(ctx):
     server_id = ctx.guild.id
     user = ctx.author
 
-    # Check which queue the player is in
-    if server_id in queue_pc and user in queue_pc[server_id]:
-        queue_pc[server_id].remove(user)
-        await ctx.send(f"{user.mention} has left the PC queue.")
-    elif server_id in queue_console and user in queue_console[server_id]:
-        queue_console[server_id].remove(user)
-        await ctx.send(f"{user.mention} has left the Console queue.")
-    else:
-        await ctx.send(f"{user.mention}, you are not in any queue.")
+    # Fetch player's region
+    player = check_profile(server_id, user)
+    if not player:
+        await ctx.send("No user profile, make one with !setup_profile")
+        return
 
+    major_region = player.region[:2].lower()
+    if major_region not in list_of_major_regions:
+        await ctx.send("Invalid region in profile. Please update your profile with a valid region.")
+        return
+
+    left_queue = False
+
+    # Check which queue the player is in and remove them
+    for system in ['pc', 'console']:
+        queue = queue_regions[major_region][system]
+        if server_id in queue and user in queue[server_id]:
+            queue[server_id].remove(user)
+            await ctx.send(f"{user.mention} has left the {system.upper()} queue for the {player.region} region.")
+            left_queue = True
+
+    if not left_queue:
+        await ctx.send(f"{user.mention}, you are not in any queue for the {player.region} region.")
 
 # Subcommand for listing the queue
 @Queue.command(name="List", aliases=['list', 'LIST'])
-async def List(ctx):
+async def ListQ(ctx):
     server_id = ctx.guild.id
     response = ""
 
-    # List PC queue
-    if server_id in queue_pc and queue_pc[server_id]:
-        pc_queue_list = "\n".join([str(item) for item in queue_pc[server_id]])
-        response += f"PC Queue for {ctx.guild.name}:\n{pc_queue_list}\n{len(queue_pc[server_id])}/{match_size}\n\n"
-    else:
-        response += "The PC queue is empty.\n\n"
+    queues_with_players = []
+    for region, systems in queue_regions.items():
+        for system, queue in systems.items():
+            if server_id in queue and queue[server_id]:
+                queue_list = "\n".join([str(item) for item in queue[server_id]])
+                response += f"{system.upper()} Queue for {region}:\n{queue_list}\n{len(queue[server_id])}/{match_size}\n\n"
+                queues_with_players.append(f"{region} - {system.upper()}")
 
-    # List Console queue
-    if server_id in queue_console and queue_console[server_id]:
-        console_queue_list = "\n".join([str(item) for item in queue_console[server_id]])
-        response += f"Console Queue for {ctx.guild.name}:\n{console_queue_list}\n{len(queue_console[server_id])}/{match_size}"
-    else:
-        response += "The Console queue is empty."
+    if not queues_with_players:
+        response = "All queues are empty."
 
     await ctx.send(response)
 
@@ -431,6 +461,35 @@ os.makedirs(player_data_dir, exist_ok=True)
 
 def get_player_file_path(user_id):
     return os.path.join(player_data_dir, f"{user_id}.json")
+
+
+@bot.command(name='assign_system_roles')
+@commands.has_permissions(administrator=True)
+async def assign_system_roles(ctx):
+    print("command called")
+    server_id = ctx.guild.id
+    guild = ctx.guild
+
+    for member in guild.members:
+        print("checking member:", member)
+        if not member.bot:
+            print("checking profile")
+            player = check_profile(server_id, member)
+            if player:
+                print("player found")
+                system = player.system
+                print(system)
+                if system:
+                    roles = system.split()  # In case multiple systems are provided (e.g., "PC PS")
+                    for role_name in roles:
+                        role = get(guild.roles, name=role_name)
+                        if role:
+                            await member.add_roles(role)
+                            await ctx.send(f"Assigned role {role_name} to {member.name}")
+                        else:
+                            await ctx.send(f"Role {role_name} not found on the server.")
+            else:
+                await ctx.send(f"No profile found for {member.name}")
 
 
 # Run the bot with your token
